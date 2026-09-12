c41fdedac856faaf2fb41f9cd9d0b5a776f8cd2c
Loic <>
2026-09-12 15:37:36 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -56,6 +56,7 @@ async function dealTarotWithChien() {
 
 async function removeCardProperty() {
     for (const card of transitionCards) {
+        console.log(card)
         functions.giveCardTo(card)
     }
 }
